099caedadc8eed362d7884d3a9ceab6b33ca90cf
hyeminem-Jo <konnimey@naver.com>
2022-05-04 21:13:46 +0900

    ✨ Implement scroll and click event of header and gnb

--- js/main.js
+++ js/main.js
@@ -1,28 +1,42 @@
 // 첫 페이지 스크롤 시 100vh 내려감
 // 첫 페이지에서 휠 감지로 100vh 내려감, 혹은 버튼을 누르면 100vh 내려감
 
-$('body').scroll(function () {
+var $about = $('.main-about').offset().top;
+
+// 스크롤 시 header 변경, 스크롤 버튼 사라짐
+$(window).scroll(function () {
   var st = $(this).scrollTop();
-  
-  if (st > 0) {
-    console.log(1)
-    console.log('hi')
+
+  if (st > $about - 70) {
     $('.global-header').addClass('is-scroll')
   } else {
     $('.global-header').removeClass('is-scroll')
   }
+
+  if (st > 0) {
+    $('.page-down').fadeOut();
+  } else {
+    $('.page-down').fadeIn();
+  }
 })
 
-window.addEventListener('scroll', function() {
-  console.log(1)
-  $('.global-header').addClass('is-scroll')
+// .main-visuall 배경 parallax 효과
+gsap.to(".background", {
+  backgroundPosition: `50% ${-innerHeight / 2}px`,
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".background",
+    scrub: true
+  }, 
+});
+
+// 모바일 menu 버튼 클릭 시 gnb 등장
+$('.menu').click(function() {
+  $('.trigger, .gnb').toggleClass('is-open')
+  $('.overlay').toggleClass('is-active')
 })
-// $(window).scroll(function () {
-//   var st = $(this).scrollTop();
-//   var $header = $('.global-header');
-//   if (st > 70) {
-//     $header.addClass('is-scroll');
-//   } else {
-//     $header.removeClass('is-scroll');
-//   }
-// })
+
+// page-down 버튼 클릭 시 아래로 스크롤
+$('.page-down').click(function () {
+  $('html, body').animate({scrollTop: $about}, 1000);
+})
